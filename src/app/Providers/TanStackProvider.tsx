d09808c5a0c36947b9686e9189c3862abd242c3d
sma <stepan.manukyan@codedunes.com>
2025-07-10 21:56:52 +0400

    front

--- src/app/Providers/TanStackProvider.tsx
+++ src/app/Providers/TanStackProvider.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode, FC } from "react"
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 interface Props {
   children: ReactNode
